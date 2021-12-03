7c55b704a9965594263ba11b30e833474872e0b7
wangyujie1207 <82414199@163.com>
2021-12-03 10:35:21 +0800

    test prettier rule

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,24 @@ import logo from "./logo.svg";
 import styles from "./App.module.css";
 
 function App() {
-  return (
-    <div className={styles["App"]}>
-      <header className={styles["App-header"]}>
-        <img src={logo} className={styles.AppLogo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={styles.AppLink}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={styles["App"]}>
+            <header className={styles["App-header"]}>
+                <img src={logo} className={styles.AppLogo} alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className={styles.AppLink}
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
